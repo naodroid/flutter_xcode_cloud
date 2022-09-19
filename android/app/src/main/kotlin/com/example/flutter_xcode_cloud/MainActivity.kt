f23decf3496ce9a541ff4e29b1b085485f8e07a9
naodroid <naodroid@gmail.com>
2022-09-19 18:30:13 +0900

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/flutter_xcode_cloud/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_xcode_cloud
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ ios/ci_scripts/ci_post_clone.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# install cocoapods
+brew install cocoapods
+
+# download flutter
+if [ ! -e "flutter" ]; then
+    curl -O https://storage.googleapis.com/flutter_infra_release/releases/stable/macos/flutter_macos_3.3.1-stable.zip
+    unzip flutter_macos_3.3.1-stable.zip > /dev/null
+    # curl -O https://storage.googleapis.com/flutter_infra_release/releases/stable/macos/flutter_macos_arm64_3.3.1-stable.zip
+    # unzip flutter_macos_arm64_3.3.1-stable.zip > /dev/null
+    export PATH="$PATH:`pwd`/flutter/bin"
+fi
+
+cd ..
+cd ..
+
+flutter build ios
